10d6d9a562bce04d36be3771749329594c97ca7f
Max <riabytsa@gmail.com>
2022-06-26 18:06:52 +0300

    Added constants.py file

---
+++ constants.py
@@ -0,0 +1,34 @@
+DB_NAME = "xxxxxxxxxxxxx"
+HOST = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.rds.amazonaws.com"
+PORT = "xxxx"
+USERNAME = "xxxxxxxxxx"
+PASSWORD = "xxxxxxxxxxxxxxxxxxxxxxx"
+
+REQUIREMENTS = [[["sql", "structured query language"], ["database"]],
+                [["python"], ["pandas"], ["matplotlib"], ["seaborn"], ["plotly"], ["numpy"], ["selenium"], ["scrapy"],
+                 ["bs4", "beautiful soup", "beautifulsoup"], ["scipy"], ["tensorflow"],
+                 ["keras"], ["pytorch"], ["scikit-learn", "scikit learn"], ["nltk"]],
+                [["machine learning with python"], ["tensorflow"], ["keras"], ["pytorch"],
+                 ["scikit-learn"]],
+                [["r"], ["ggplot2"], ["r shiny"]],
+                [["programming languages"], ["python"], ["r"], ["scala"], ["matlab"],
+                 ["java"], ["c++"], ["c#", ".net"], ["javascript"], ["golang"]],
+                [["data visualization"], ["matplotlib"], ["seaborn"], ["plotly"], ["ggplot2"], ["tableau"],
+                 ["power bi", "powerbi", "bi"]],
+                [["statistical software"], ["sas"], ["spss"], ["stata"]],
+                [["cloud technologies"], ["aws"], ["azure"], ["google cloud", "google cloud platform", "google"]],
+                [["data mining"], ["web scraping"], ["selenium"], ["scrapy"],
+                 ["bs4", "beautiful soup", "beautifulsoup"], ["api"]],
+                [["api development"], ["rest api", "restful api"], ["postman"]],
+                [["nosql"], ["bigtable"], ["dynamodb"], ["mongodb"], ["cassandra"], ["hbase"], ["json"]],
+                [["data warehousing"], ["dbt", "data build tool"], ["hive"]],
+                [["data pipeline"], ["kafka"], ["airflow"], ["spark", "pyspark"]],
+                [["distributed computing"], ["hadoop"], ["mapreduce"], ["hive"], ["hbase"], ["spark", "pyspark"]],
+                [["microsoft office"], ["excel"], ["powerpoint"], ["ms word", "microsoft word"]],
+                [["application deployment"], ["docker", "dockerizing", "dockerization"]],
+                [["git"], ["version control"], ["github"]],
+                [["ai"], ["deep learning", "neural network", "neural networks"], ["computer vision"], ["regression"],
+                 ["nlp", "natural language processing", "nltk"]]]
+
+EXPERIENCE_LEVELS = ["Entry level", "Mid level", "Senior level"]
+DEGREES = ["No degree", "Bachelor", "Master", "PhD"]
